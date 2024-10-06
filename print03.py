c57ad0bdca8b6843eb887db5475189f9e2e63245
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-06 12:28:02 +0300

    range

---
+++ print03.py
@@ -0,0 +1,17 @@
+print(7+2*3) #13
+print((7+2)*3) #27
+
+print(range(8)) 
+print(*range(8))   #8'e kadar olan sayıları yazar sekiz dahil değildir.
+print(*range(5,20))
+print(*range(5,20,2)) #5'ten yirmiye kadar ikişerli artan sayıları yazar.
+print(*range(20,5,-2)) #20'den beşe kadar ikişerli azalan sayıları yazar.
+print(range(20,5,-2))
+
+aaa='Ankara'
+print(aaa)
+print(len(aaa))  #length yani uzunluk demektir kaç harf oldupunu belirtir.
+print(aaa,'ifadesinin uzunluğu', len (aaa),'karakterdir')  #ankara ifadesinin uzulluğu 6 karakterdir yazmış oldun.
+
+
+input()
