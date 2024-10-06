b8c8a3a9f042dcdea3fd69b2bc7d0d5a8cf145e6
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-06 13:09:40 +0300

    deneme1

---
+++ proje-01.py
@@ -0,0 +1,39 @@
+
+         ╔═══════════════════════╗
+print('     HESAP MAKİNESİ         ')
+    
+
+print('     1- Toplama               ')
+      
+print('     2- Çarpma                ')
+
+print('     3- Çıkarma               ') 
+
+         ╚═══════════════════════╝
+
+
+
+
+
+
+
+
+
+
+
+
+secim = input ()
+
+
+print(secim,'seçtiniz.')
+if secim == '1' :
+      print('1 seçtiniz, toplama yapılacak.')
+if secim == '2' :
+      print ('2 seçtiniz, çarpma yapılacak.')
+
+
+
+ 
+
+
+
