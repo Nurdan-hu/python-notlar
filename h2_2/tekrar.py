929810eb4a2b0f9cfe59f3513bfb74d255be468d
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-13 10:56:30 +0300

    op örnek

---
+++ h2_2/tekrar.py
@@ -0,0 +1,12 @@
+fiyat = 10
+fiyat = fiyat + 5    #son söylediğimiz geçerlidir.
+kdv = 20
+print(fiyat*kdv/100)
+
+a = 5
+b = 5
+c = 4
+d = 7
+print (a==b and c==d)  #c d'ye eşit olmadığı için false çıkar.
+print (a==b and c<d)   #True
+print(a<d)
