90c61106c5156920700b9b67a21d632103659a97
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-13 12:31:16 +0300

    a

---
+++ print/print01.py
@@ -0,0 +1,12 @@
+print('Merhaba' *5)
+print(4*9)
+print('4*9')
+abc = 42    
+abc = 36    #abc bir değişkendir
+print(abc)
+#hashtag bilgi vermek için kullanılır abc'nin başına hashtag koyarsam devre dışı olur
+print('Üç tane kitap aldım')
+print(9-2*8)
+lm = abc
+abc = 11
+print(lm)
---
+++ print/print02.py
@@ -0,0 +1,23 @@
+ad= 'Nurdan'
+print('Merhaba', ad)
+print('Merhaba'+ad)
+print('''Eren'in annesi gel dedi''')
+print('Eren\'in annesi gel dedi')   # kesme işaretinin yanına slaş koyarsan cümle hata vrmez.
+print('Merhaba', ad)
+input() # input koymazsan ekranda görünür ve kaybolur. İnput koyduğun için diğer komut ekrana bir şey yazmadan gelmez.
+print('BİLİŞİM \tTEKNOLOJİLERİ \ndersi: \nsınıfı: \nnumarası:')   #\t büyük boşluk bırakır, \n alt satıra geçer.
+
+
+abc= 800
+print(abc)
+print('abc')
+# print(ABC) #hata verir abcABC'ye eşit değildir.
+
+sayı='488'
+# print(sayı+2) #hata verir metin ve sayı bir arada olmaz.
+print(sayı+'2')
+
+a=5
+b=4
+print('a'+'b')
+print(a+b)
