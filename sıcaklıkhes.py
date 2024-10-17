3c0027c4d6e17f9ae26a5e910b37b5106339c248
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-17 22:24:09 +0300

    d

---
+++ sıcaklıkhes.py
@@ -0,0 +1,7 @@
+derece = float(input("Dereceyi giriniz:"))
+birim = input("Celcius: C, Fahrenheit: F, Kelvin: K: ")
+
+if birim == "C":
+    print (input("Hangi birime çevirmek istersiniz?"))
+if birim == "K":
+    print(derece - 273)
