0b71afd83b720e5087cba70af12b5cbf50496e36
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-19 11:23:29 +0300

    ı

--- proje1/proje-01.py
+++ proje1/proje-01.py
@@ -2,19 +2,27 @@ import hesapmaktry_01
 import calender
 import BMIhesaplama
 import sıcaklıkhes
-print("╔═════════════════════╗")
-#print("║\033[1;31;40m   HESAP MAKİNESİ  \033[1;32;40m  ║")
-print("║   TTB APP           ║")
-print("║                     ║")
-print("║  1-Hesap makinesi   ║")
-print("║  2-Takvim           ║")
-print("║  3-BMI hesapla      ║")
-print("║  4-Sıcaklık çevirme ║")
-print("║    Seçimiz nedir?   ║")
-print("╚═════════════════════╝")
+def anamenu():
+    print("╔═════════════════════╗")
+    #print("║\033[1;31;40m   HESAP MAKİNESİ  \033[1;32;40m  ║")
+    print("║   TTB APP           ║")
+    print("║                     ║")
+    print("║  1-Hesap makinesi   ║")
+    print("║  2-Takvim           ║")
+    print("║  3-BMI hesapla      ║")
+    print("║  4-Sıcaklık çevirme ║")
+    print("║  5-Oyun             ║")
+    print("║    Seçimiz nedir?   ║")
+    print("╚═════════════════════╝")
 
-secim = input ()
-if secim == "1" : hesapmaktry_01.hesaplarmenu()
-if secim == "2" : calender.takvım()
-if secim == "3" : BMIhesaplama.BMI()
-if secim == "4" : sıcaklıkhes.sıcaklık()
+    secim = input ()
+    if secim == "1" :
+        hesapmaktry_01.hesaplarmenu()
+        anamenu()
+    if secim == "2" :
+        calender.takvım()
+    if secim == "3" :
+        BMIhesaplama.BMI()
+    if secim == "4" :
+        sıcaklıkhes.sıcaklık()  
+anamenu()
