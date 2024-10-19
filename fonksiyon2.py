98cdf8dc23a9f4c358ae73f397257ce64a382e4a
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-19 12:47:54 +0300

    x

---
+++ fonksiyon2.py
@@ -0,0 +1,6 @@
+def topla (s1=0, s2=0, s3=0, s4=0) :   #sayılara sıfır atamamız eğer o sayıya bir değer girilmezse sıfır olarak kabul edileceği anlamına gelir.
+    toplam = s1 + s2 + s3 + s4 
+    return toplam
+print(topla(3,4,))
+print(topla(3,4,8))
+print (topla())
---
+++ for1.py
@@ -0,0 +1,20 @@
+#for a in range(10):
+   #print(a,".sayı", sep="") #sep koymqmızın sebebi rakam ile sayının arasında boşluk olmasını istememem.
+
+#for b in range(5,20): print (b)
+
+
+
+#for c in range(10,30,2): print(c)
+
+#for zzz in range(100,10,-10): print(zzz)
+#for x in 'Erdinç': print(x)
+#for x in "Selam": print(x, end="\n")
+
+#kelime = "Merhaba"
+#for x in kelime: print(x)
+
+for a in range (1,11):
+    for b in range (1,11):
+        print(a,"x",b,"=",a*b)
+    print()                                   #çarpım tablosu kodu
