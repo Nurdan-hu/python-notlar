e39c76037e50f138fcccd031595b4c3e5cfe60e8
Nurdan-hu <nurdan.ankarali2009@gmail.com>
2024-10-26 10:38:04 +0300

    klasordüzenleme

---
+++ string,op,math/operatörler.py
@@ -0,0 +1,19 @@
+aa = 5
+bb = 4
+cc = '5'
+dd = '4'
+print (aa+bb)
+print (cc+dd)
+#print (aa+dd) hata verir çünkü biri metindir diğeri ise sayı.
+print(aa*bb)
+print(aa*dd) #biri metin diğeri sayı olduğu için metin tekrar eder.
+#print(cc*dd)  hata verir çünkü ikisi de metindir.
+
+print(10/3)
+print(10//3)#küsüratı saymaz,yuvarlama yapmaz.
+
+print(10**3)
+print(2**3) #üs alma demektir.
+
+print(10%3) #kalanı yazar.
+print(5%3)
